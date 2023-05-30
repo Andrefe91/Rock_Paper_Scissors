3c786b1c6e250980553f5df9fa803f8bdc265b21
Andres Felipe Munoz <109099541+Andrefe91@users.noreply.github.com>
2023-05-30 04:32:15 +0000

    Adds the rest of the functions and main logic for the excersice

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -12,28 +12,77 @@ function getComputerChoice() {
     }
 }
 
-function playGame(playerSelection, computerChoice) {
-    if (playerSelection === computerChoice) {
+function playRound(playerSelection, getComputerChoice) {
+    
+    let computerChoice = getComputerChoice();
+    console.log(`Computer Choice is: ${computerChoice}`);
+
+    /* This transform the player selection text to lowercase*/
+    playerSelection = playerSelection.toLowerCase();
+
+    console.log(`Player Choice is: ${playerSelection}`);
+
+    if (playerSelection == computerChoice) {
         console.log('It\'s a tie!');
-    } else if (playerSelection === 'rock') {
-        if (computerChoice ==='scissors') {
-            console.log('You win!');
-        } else {
-            console.log('You lose!');
+        return 0;
+    } else if (playerSelection == 'rock') {
+        if(computerChoice == 'scissors'){
+            console.log('You Win !!');
+            return 1;
+        } else{
+            console.log('You Lose !! T.T');
+            return 0;
         }
-    } else if (playerSelection ==='scissors') {
-        if (computerChoice === 'paper') {
-            console.log('You win!');
-        } else {
-            console.log('You lose!');
-        }
+    } else if (playerSelection == 'scissors'){
+        if(computerChoice == 'paper'){
+            console.log('You Win !!');
+            return 1;
+        } else{
+            console.log('You Lose !! T.T');
+            return 0;
+        }       
+    } else if (playerSelection == 'paper'){
+        if(computerChoice == 'rock'){
+            console.log('You Win !!');
+            return 1;
+        } else{
+            console.log('You Lose !! T.T');
+            return 0;
+        }           
     } else {
-        if (computerChoice === 'rock') {
-            console.log('You win!');
-        } else {
-            console.log('You lose!');
-        }
+        alert('Error !!');
+        return 0;
+    }
+
+}
+
+function playGame(){
+    let result = 0;
+    let playerSelection = prompt('Player, your choice ?:')
+
+    if (playerSelection == NaN || playerSelection == null){
+        result = 0;
+        console.log('No option selected, given score is 0');
+    } else {
+        result = playRound(playerSelection, getComputerChoice);
+    }
+
+    return result;
+}
+
+
+function play(){
+    let result = 0;
+
+    for (let i= 1; i <= 5; i++){
+        result += playGame();
+    }
+
+    if (result >= 3){
+        console.log('You Won the Game !!');
+    } else {
+        console.log("Sorry, you lost the game T.T x 2")
     }
 }
 
-playGame('rock', getComputerChoice);
+play();
