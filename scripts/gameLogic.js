07d7417d3aaa4a85f90af08d998738106d3c055f
Andres Felipe Munoz <109099541+Andrefe91@users.noreply.github.com>
2023-06-12 03:16:27 +0000

    Integrates the ui and logic for the rock, paper and Scissor game

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -10,11 +10,11 @@ function getComputerChoice() {
     } else {
         return 'paper';
     }
+
 }
 
-function playRound(playerSelection, getComputerChoice) {
+function playRound(playerSelection, computerChoice) {
     
-    let computerChoice = getComputerChoice();
     console.log(`Computer Choice is: ${computerChoice}`);
 
     /* This transform the player selection text to lowercase*/
@@ -23,31 +23,31 @@ function playRound(playerSelection, getComputerChoice) {
     console.log(`Player Choice is: ${playerSelection}`);
 
     if (playerSelection == computerChoice) {
-        console.log('It\'s a tie!');
-        return 0;
+        console.log('It\'s a tie this Round !');
+        return [0,'It\'s a tie this Round !'];
     } else if (playerSelection == 'rock') {
         if(computerChoice == 'scissors'){
-            console.log('You Win !!');
-            return 1;
+            console.log('You Win this Round !!');
+            return [1,'You Win this Round !!'];
         } else{
-            console.log('You Lose !! T.T');
-            return 0;
+            console.log('You Lose this Round !! T.T');
+            return [0,'You Lose this Round !! T.T'];
         }
     } else if (playerSelection == 'scissors'){
         if(computerChoice == 'paper'){
-            console.log('You Win !!');
-            return 1;
+            console.log('You Win this Round !!');
+            return [1,'You Win this Round !!'];
         } else{
-            console.log('You Lose !! T.T');
-            return 0;
+            console.log('You Lose this Round !! T.T');
+            return [0,'You Lose this Round !! T.T'];
         }       
     } else if (playerSelection == 'paper'){
         if(computerChoice == 'rock'){
-            console.log('You Win !!');
-            return 1;
+            console.log('You Win this Round !!');
+            return [1,'You Win this Round !!'];
         } else{
-            console.log('You Lose !! T.T');
-            return 0;
+            console.log('You Lose this Round !! T.T');
+            return [0,'You Lose this Round !! T.T'];
         }           
     } else {
         alert('Error !!');
@@ -56,33 +56,137 @@ function playRound(playerSelection, getComputerChoice) {
 
 }
 
-function playGame(){
-    let result = 0;
-    let playerSelection = prompt('Player, your choice ?:')
 
-    if (playerSelection == NaN || playerSelection == null){
-        result = 0;
-        console.log('No option selected, given score is 0');
-    } else {
-        result = playRound(playerSelection, getComputerChoice);
-    }
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener('click',playSelection));
 
-    return result;
+function playSelection(event) {
+
+    let playerSelection = event.target.getAttribute("data-action");
+    let computerChoice = getComputerChoice();
+
+    /*Little animation to the back ground color via CSS*/
+    event.target.classList.add('pressed');
+    setTimeout(() => {
+        event.target.classList.remove('pressed');
+    },100)
+
+    const computerChoiceField = document.querySelector('#computerSelection');
+    computerChoiceField.textContent = computerChoice;
+
+    /*We toggle the class so the text change color depending of the result*/
+    toggleClase(computerChoiceField, computerChoice);
+
+    console.log(computerChoice);
+    result = playRound(playerSelection, computerChoice);
+    console.log(result);
+    resultsOfGame(result);
+    gameNumber();
 }
 
+function toggleClase (object, classGiven) {
 
-function play(){
-    let result = 0;
-
-    for (let i= 1; i <= 5; i++){
-        result += playGame();
+    /*We must delete the current class for the value*/
+    if(object.classList.value == ""){
+    
+    } else{
+        object.classList.toggle(object.classList.value);
     }
 
-    if (result >= 3){
+    /*So we can now add another*/
+    object.classList.add(classGiven);
+}
+
+function resultsOfGame (result){
+    const resultField = document.querySelector('#result');
+    resultField.textContent = result[1];
+
+    let classGiven = 'win';
+    console.log(result[0]);
+
+    if (result[0] == 0){
+        classGiven = 'loose'
+    }
+
+    toggleClase(resultField,classGiven);
+    keepScore(result[0]);
+}
+
+function keepScore(score){
+    const scoreField = document.querySelector('#score');
+    let oldScore = Number(scoreField.textContent);
+    oldScore += score;
+    
+    scoreField.textContent = oldScore;
+}
+
+function gameNumber(){
+    const gameField = document.querySelector('#gameNumber');
+    let gameNumber = Number(gameField.textContent);
+
+    gameNumber += 1;
+
+    gameField.textContent = gameNumber;
+
+    if (gameNumber == 5){
+        printWinner(gameField);
+    }
+
+}
+
+function printWinner(){
+    
+    const scoreField = document.querySelector('#score');
+    let score = Number(scoreField.textContent);
+
+    const resultText = document.querySelector('#result');
+
+    if (score >= 3){
         console.log('You Won the Game !!');
+        resultText.textContent = 'You Won the Game !!'
+        toggleClase(resultText,'win');
     } else {
         console.log("Sorry, you lost the game T.T x 2")
+        resultText.textContent = 'Sorry, you lost the game T.T'
+        toggleClase(resultText,'loose');
     }
+
+    buttons.forEach(button => button.removeEventListener('click',playSelection));
+    restart();
 }
 
-play();
+function restart (){
+    let container = document.querySelector('div.buttonRestart');
+    
+    const resetButton = document.createElement('button');
+    resetButton.classList.add('gameAction','reset');
+    resetButton.textContent = 'Reload'
+    container.appendChild(resetButton);
+
+    resetButton.addEventListener('click',reset);
+}
+
+function reset(){
+
+    /*So te person can only reset once per game*/
+    let resetButton = document.querySelector('.gameAction.reset');
+    resetButton.removeEventListener('click',reset);
+    resetButton.remove();
+
+    /* It's neccesary to clean the board!*/
+    let textField = []
+    textField[0] = document.querySelector('#score');
+    textField[1] = document.querySelector('#gameNumber');
+    textField[2] = document.querySelector('#result');
+    textField[3] = document.querySelector('#computerSelection');
+
+    textField[0].textContent = '0';
+    textField[1].textContent = '0';
+    textField[2].textContent = '';
+    textField[3].textContent = '';
+
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(button => button.addEventListener('click',playSelection));
+
+    
+}
